aa85b6dedb3c1327f40bb8fedaf994f6b53fc11a
Vilius Sutkus <ViliusSutkus89@gmail.com>
2019-06-15 13:34:00 +0300

    Silence libiconv search message, search is done to decide whether to build it, not whether to use it

--- 3rdparty/packages/iconv.cmake
+++ 3rdparty/packages/iconv.cmake
@@ -2,7 +2,7 @@ include_guard(GLOBAL)
 
 # libiconv doesn't provide pkg-config .pc
 # Check if build sneeded, before calling to EPAutotools
-find_package(Iconv)
+find_package(Iconv QUIET)
 
 # Android >= 28 provides iconv
 IF ((NOT Iconv_FOUND) AND NOT (ANDROID_NATIVE_API_LEVEL GREATER_EQUAL 28))
