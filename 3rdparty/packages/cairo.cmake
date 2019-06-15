d784036a2e972c8e07be5a79bfd5efb551f0015b
Vilius Sutkus <ViliusSutkus89@gmail.com>
2019-06-15 17:25:03 +0300

    Remove leftover URL to local file in cairo.cmake package

--- 3rdparty/packages/cairo.cmake
+++ 3rdparty/packages/cairo.cmake
@@ -3,6 +3,5 @@ include_guard(GLOBAL)
 ExternalProjectAutotools(cairo
   DEPENDS libpng pixman-1 freetype glib-2.0 iconv
   URL https://cairographics.org/releases/cairo-1.16.0.tar.xz
-  #URL ${TARBALL_STORAGE}/cairo-1.16.0.tar.xz
   URL_HASH SHA256=5e7b29b3f113ef870d1e3ecf8adf21f923396401604bda16d44be45e66052331
 )
