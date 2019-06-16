ae7bbeca2ff49ada404f37cbb68a163b4f7cc227
Vilius Sutkus <ViliusSutkus89@gmail.com>
2019-06-16 04:39:07 +0300

    Update Android packaging script

--- build_android.sh
+++ build_android.sh
@@ -17,8 +17,6 @@ if [ ! -d "$LIB" ]; then
   mkdir --parents --verbose $LIB/lib/src/main
 
   ln --symbolic --verbose $APP/gradle $LIB/gradle
-  # Do I need this?
-  ln --symbolic --verbose $APP/android.iml $LIB/android.iml
   ln --symbolic --verbose $APP/build.gradle $LIB/build.gradle
   ln --symbolic --verbose $APP/gradle.properties $LIB/gradle.properties
   ln --symbolic --verbose $APP/gradlew $LIB/gradlew
@@ -53,18 +51,22 @@ fi
 
 cd $LIB
 ./gradlew assemble
-for build_type_and_abi in $THIRD_PARTY_DIR/built/cmake/*/*/
-do
+for build_type in $THIRD_PARTY_DIR/built/cmake/*; do
+  for build_type_and_abi in $build_type/*; do
   echo "Building LIB: $build_type_and_abi"
   cmake --build $build_type_and_abi
 done
 
 cd $APP
 ./gradlew assemble
-for build_type_and_abi in $THIRD_PARTY_DIR/.externalNativeBuild/cmake/*/*/
-do
-  echo "Building APP: $build_type_and_abi"
-  cmake --build $build_type_and_abi
-  #@TODO:
-  cmake --build $build_type_and_abi --target install
+for build_type in $APP/app/.externalNativeBuild/cmake/*; do
+  for build_type_and_abi in $build_type/*; do
+    cmake --build $build_type_and_abi --target install
+
+    mkdir $build_type_and_abi/built/sample_pdfs
+    cp -v $BASEDIR/test/browser_tests/*.pdf $build_type_and_abi/built/sample_pdfs/
+    tar -cf $build_type_and_abi/pdf2htmlEX.tar --directory $build_type_and_abi built
+    tar --list -f $build_type_and_abi/pdf2htmlEX.tar
+    echo "$build_type_and_abi/pdf2htmlEX.tar is ready!"
+    done
 done
