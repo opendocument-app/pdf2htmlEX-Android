d73050d940bfd166210932902cd8a13422e7ff36
Vilius Sutkus <ViliusSutkus89@gmail.com>
2019-06-21 03:29:52 +0300

    Disable ARM SIMD and Neon instructions in Pixman package, fails to build for armeabi-v7a

--- 3rdparty/packages/pixman-1.cmake
+++ 3rdparty/packages/pixman-1.cmake
@@ -19,5 +19,8 @@ ExternalProjectMeson(pixman-1
   DEPENDS libpng glib-2.0
   URL https://cairographics.org/releases/pixman-0.38.4.tar.gz
   URL_HASH SHA256=da66d6fd6e40aee70f7bd02e4f8f76fc3f006ec879d346bae6a723025cfbdde7
+  CONFIGURE_ARGUMENTS
+    #@TODO: enable for arm64. these fail only for 32 bit arm.
+    -Dneon=disabled -Darm-simd=disabled
   ${PIXMAN_SHARED_ARGUMENT}
 )
