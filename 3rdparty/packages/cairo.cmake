c4acee998507cbf2f8d3c4c8d1f84a44a0c63d2a
Vilius Sutkus <ViliusSutkus89@gmail.com>
2019-09-16 16:19:54 +0000

    Upgrade Cairo to 1.17.2

--- 3rdparty/packages/cairo.cmake
+++ 3rdparty/packages/cairo.cmake
@@ -2,6 +2,6 @@ include_guard(GLOBAL)
 
 ExternalProjectAutotools(cairo
   DEPENDS freetype glib-2.0 iconv libpng pixman-1
-  URL https://cairographics.org/releases/cairo-1.16.0.tar.xz
-  URL_HASH SHA256=5e7b29b3f113ef870d1e3ecf8adf21f923396401604bda16d44be45e66052331
+  URL https://cairographics.org/snapshots/cairo-1.17.2.tar.xz
+  URL_HASH SHA256=6b70d4655e2a47a22b101c666f4b29ba746eda4aa8a0f7255b32b2e9408801df
 )
