a662c795b5a3516de3af212105d0f4be048f6e14
Vilius Sutkus <ViliusSutkus89@gmail.com>
2019-09-26 21:53:33 +0000

    Enable libopenjp2

--- 3rdparty/packages/FixLibopenjp2Source.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -euo pipefail
-OPENJPE2_SRC=$1
-
-# Force linking against libjpeg when linking against libtiff
-sed -i 's/${TIFF_LIBNAME}/${TIFF_LIBNAME} ${CMAKE_INSTALL_PREFIX}\/lib\/${CMAKE_STATIC_LIBRARY_PREFIX}jpeg${CMAKE_STATIC_LIBRARY_SUFFIX}/g' $OPENJPE2_SRC/src/bin/jp2/CMakeLists.txt
---
+++ 3rdparty/packages/libopenjp2-Patch-Source.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+set -euo pipefail
+
+# Force linking against libjpeg when linking against libtiff
+sed -i 's/${TIFF_LIBNAME}/${TIFF_LIBNAME} ${CMAKE_INSTALL_PREFIX}\/lib\/${CMAKE_STATIC_LIBRARY_PREFIX}jpeg${CMAKE_STATIC_LIBRARY_SUFFIX}/g' $1/src/bin/jp2/CMakeLists.txt
+
