140100fe2006a52446d801af3301edbc9bd2d4a3
Vilius Sutkus <ViliusSutkus89@gmail.com>
2019-09-26 21:32:28 +0000

    Update glib patch script. iconv and intl finder patches not needed anymore

--- 3rdparty/packages/glib-2.0.cmake
+++ 3rdparty/packages/glib-2.0.cmake
@@ -5,10 +5,5 @@ ExternalProjectMeson(glib-2.0
   URL https://ftp.gnome.org/pub/gnome/sources/glib/2.61/glib-2.61.1.tar.xz
   URL_HASH SHA256=f8d827955f0d8e197ff5c2105dd6ac4f6b63d15cd021eb1de66534c92a762161
   CONFIGURE_ARGUMENTS -Dlibmount=false
-
-  EXTRA_ARGUMENTS
-  UPDATE_COMMAND
-    ${CMAKE_CURRENT_SOURCE_DIR}/packages/FixGlibSource.sh
-    ${CMAKE_CURRENT_BINARY_DIR}/glib-2.0-prefix/src/glib-2.0/
-    ${ANDROID_NATIVE_API_LEVEL}
 )
+
