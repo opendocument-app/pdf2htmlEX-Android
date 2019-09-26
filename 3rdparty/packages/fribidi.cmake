dd086c6cd0d738d2b24854dbf13a3375e8799018
Vilius Sutkus <ViliusSutkus89@gmail.com>
2019-09-26 21:50:48 +0000

    Add Fribidi package

---
+++ 3rdparty/packages/fribidi.cmake
@@ -0,0 +1,7 @@
+include_guard(GLOBAL)
+
+ExternalProjectAutotools(fribidi
+  URL https://github.com/fribidi/fribidi/releases/download/v1.0.5/fribidi-1.0.5.tar.bz2
+  URL_HASH SHA256=6a64f2a687f5c4f203a46fa659f43dd43d1f8b845df8d723107e8a7e6158e4ce
+)
+
