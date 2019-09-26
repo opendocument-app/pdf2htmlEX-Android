d488d0826154b2639f883db1111b27689fe56f45
Vilius Sutkus <ViliusSutkus89@gmail.com>
2019-09-26 21:13:57 +0000

    Link Freetype against libpng

--- 3rdparty/packages/freetype.cmake
+++ 3rdparty/packages/freetype.cmake
@@ -1,13 +1,12 @@
 include_guard(GLOBAL)
 
-ExternalProjectCMake(freetype
-  DEPENDS zlib
+ExternalProjectAutotools(freetype
+  DEPENDS libpng zlib
   URL https://download.savannah.gnu.org/releases/freetype/freetype-2.10.1.tar.xz
   URL_HASH SHA256=16dbfa488a21fe827dc27eaf708f42f7aa3bb997d745d31a19781628c36ba26f
 
-  # Freetype needs some "patching" after the install for Cairo to pick it up properly.
-  # All three patched documented in the script.
-  EXTRA_ARGUMENTS
-    TEST_COMMAND ${CMAKE_CURRENT_SOURCE_DIR}/packages/FixFreetypeInstall.sh ${THIRDPARTY_PREFIX}
-    LOG_TEST 1
+  CONFIGURE_ARGUMENTS --with-zlib=yes --with-bzip2=no
+    --with-png=yes
+    --with-harfbuzz=no
 )
+
