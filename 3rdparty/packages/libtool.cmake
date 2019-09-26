75cf6e477c9a0816f6269933c55e514b9a09b29e
Vilius Sutkus <ViliusSutkus89@gmail.com>
2019-09-26 21:55:27 +0000

    Add libtool ( a.k.a. libltdl ), needed by older version of fontforge

---
+++ 3rdparty/packages/libtool.cmake
@@ -0,0 +1,7 @@
+include_guard(GLOBAL)
+
+ExternalProjectAutotools(libtool
+  URL http://ftpmirror.gnu.org/libtool/libtool-2.4.6.tar.xz
+  URL_HASH SHA512=a6eef35f3cbccf2c9e2667f44a476ebc80ab888725eb768e91a3a6c33b8c931afc46eb23efaee76c8696d3e4eed74ab1c71157bcb924f38ee912c8a90a6521a4
+)
+
