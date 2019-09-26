1d771401d8657c9c3f43b3284e7b9c46a808c9f1
Vilius Sutkus <ViliusSutkus89@gmail.com>
2019-09-26 21:03:32 +0000

    Update iconv. EPHelper function already has a check for iconv, don't check in Package

--- 3rdparty/packages/iconv.cmake
+++ 3rdparty/packages/iconv.cmake
@@ -1,21 +1,7 @@
 include_guard(GLOBAL)
 
-# iconv not needed on ANDROID-28+
-IF (ANDROID_NATIVE_API_LEVEL GREATER_EQUAL 28)
-  return()
-endif()
-
-# libiconv doesn't provide pkg-config .pc
-# Check if build sneeded, before calling to EPAutotools
-if (EXISTS ${THIRDPARTY_PREFIX}/lib/${CMAKE_STATIC_LIBRARY_PREFIX}iconv${CMAKE_STATIC_LIBRARY_SUFFIX})
-  return()
-endif()
-
 ExternalProjectAutotools(iconv
   URL https://ftp.gnu.org/pub/gnu/libiconv/libiconv-1.16.tar.gz
   URL_HASH SHA256=e6a1b1b589654277ee790cce3734f07876ac4ccfaecbee8afa0b649cf529cc04
-  EXTRA_ARGUMENTS
-    # libiconv does not provide libiconv.pc
-    TEST_COMMAND ${CMAKE_CURRENT_SOURCE_DIR}/packages/FixIconvInstall.sh ${THIRDPARTY_PREFIX}
-    LOG_TEST 1
 )
+
