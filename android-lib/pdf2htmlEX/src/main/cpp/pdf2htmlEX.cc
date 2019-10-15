5386b3fb96b27006d0dcbae86f3cf246fafa9b36
Vilius Sutkus <ViliusSutkus89@gmail.com>
2019-10-15 00:39:46 +0000

    [WIP] Convert gradle App project to Android library, it builds 3rdparty libs too.

---
+++ android-lib/pdf2htmlEX/src/main/cpp/pdf2htmlEX.cc
@@ -0,0 +1,122 @@
+#include <jni.h>
+#include <string>
+#include <vector>
+//#include "pdf2htmlEX/pdf2htmlEX.h"
+
+// Creating char ** by hand is rather annoying.
+// I'll rather take vector<string> and convert it before calling.
+void vector_to_char_pp(const std::vector<const std::string> & input, int * argc, char *** argv) {
+  *argv = nullptr;
+  *argc = 0;
+
+  size_t sz = input.size();
+  if (sz == 0) {
+    return;
+  }
+  char ** output = new char *[sz];
+  int i = 0;
+  for (const std::string & s: input) {
+    size_t len = s.length();
+    output[i] = new char[len+1];
+    output[i][len] = '\0';
+    strncpy(output[i], s.c_str(), len);
+    i++;
+  }
+
+  *argc = static_cast<int>(sz);
+  *argv = output;
+}
+
+extern "C"
+JNIEXPORT void JNICALL
+Java_com_viliussutkus89_pdf2htmlex_android_1sample_1app_MainActivity_redirect(JNIEnv *env,
+                                                                            jobject,
+                                                                            jstring stdout_,
+                                                                            jstring stderr_) {
+    const char * stdout_c = env->GetStringUTFChars(stdout_, 0);
+    const char * stderr_c = env->GetStringUTFChars(stderr_, 0);
+
+    freopen(stdout_c, "w", stdout);
+    setvbuf(stdout, 0, _IOLBF, 0);
+    fprintf(stdout, "stdout freopened\n");
+
+    freopen(stderr_c, "w", stderr);
+    setvbuf(stderr, 0, _IONBF, 0);
+    fprintf(stderr, "stderr freopened\n");
+
+    env->ReleaseStringUTFChars(stdout_, stdout_c);
+    env->ReleaseStringUTFChars(stderr_, stderr_c);
+}
+
+extern "C"
+JNIEXPORT void JNICALL
+Java_com_viliussutkus89_pdf2htmlex_android_1sample_1app_MainActivity_setenv(JNIEnv *env,
+                                                                            jobject,
+                                                                            jstring homedir_,
+                                                                            jstring tmpdir_
+                                                                            ) {
+    const char * homedir = env->GetStringUTFChars(homedir_, 0);
+    const char * tmpdir = env->GetStringUTFChars(tmpdir_, 0);
+
+    setenv("HOME", homedir, 1);
+    setenv("TMPDIR", tmpdir, 1);
+    setenv("USER", "HELLO!!", 1);
+
+    FILE *x = tmpfile();
+
+    env->ReleaseStringUTFChars(tmpdir_, tmpdir);
+}
+
+extern "C"
+JNIEXPORT jint JNICALL
+Java_com_viliussutkus89_pdf2htmlex_android_1sample_1app_MainActivity_call_1pdf2htmlEX(JNIEnv *env,
+                                                                                      jobject,
+                                                                                      jstring dataDir_,
+                                                                                      jstring popplerDir_,
+                                                                                      jstring tmpDir_,
+                                                                                      jstring inputFile_,
+                                                                                      jstring outputFile_) {
+    const char *dataDir = env->GetStringUTFChars(dataDir_, 0);
+    const char *popplerDir = env->GetStringUTFChars(popplerDir_, 0);
+    const char *tmpDir = env->GetStringUTFChars(tmpDir_, 0);
+    const char *inputFile = env->GetStringUTFChars(inputFile_, 0);
+    const char *outputFile = env->GetStringUTFChars(outputFile_, 0);
+
+
+    std::vector<const std::string> args = {
+        "libpdf2htmlEX.so",
+        "--data-dir", dataDir,
+        "--poppler-data-dir", popplerDir,
+        "--tmp-dir", tmpDir,
+//        "--tounicode", "1",
+//        "--correct-text-visibility", "0",
+//        "--font-format", "svg",
+//        "--auto-hint", "1",
+//        "--embed-image", "0",
+//        "--process-nontext", "0",
+//        "--process-annotation", "1",
+//        "--process-form", "1",
+//        "--fallback", "1",
+//        "--proof", "1",
+        inputFile, outputFile
+    };
+
+    int argc;
+    char ** argv;
+    vector_to_char_pp(args, &argc, &argv);
+
+    //int retVal = pdf2htmlEX_main(argc, argv);
+    int retVal = 0;
+    
+    for (int i = 0; i < argc; i++) {
+        delete[] argv[i];
+    }
+    delete argv;
+
+    env->ReleaseStringUTFChars(dataDir_, dataDir);
+    env->ReleaseStringUTFChars(popplerDir_, popplerDir);
+    env->ReleaseStringUTFChars(tmpDir_, tmpDir);
+    env->ReleaseStringUTFChars(inputFile_, inputFile);
+    env->ReleaseStringUTFChars(outputFile_, outputFile);
+    return retVal;
+}
