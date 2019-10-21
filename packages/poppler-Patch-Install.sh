18bfc33829a70bb733784df5734608607e4db922
Vilius Sutkus <ViliusSutkus89@gmail.com>
2019-10-21 15:44:29 +0000

    Include freetype in poppler's pkg-config patch

--- packages/poppler-Patch-Install.sh
+++ packages/poppler-Patch-Install.sh
@@ -6,7 +6,7 @@ PC_FILE=$THIRDPARTY_PREFIX/lib/pkgconfig/poppler.pc
 
 # Poppler uses libopenjp2 and lcms2
 # But does not declare them in poppler.pc
-echo "Requires.private: libopenjp2 lcms2 libtiff-4" >> $PC_FILE
+echo "Requires.private: libopenjp2 lcms2 libtiff-4 freetype" >> $PC_FILE
 
 # Check if sed succeeded (in case there is no Requires.private line)
 grep "lcms2" $PC_FILE --quiet
