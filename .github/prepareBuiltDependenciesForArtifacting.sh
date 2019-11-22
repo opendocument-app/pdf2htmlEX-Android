11e24b2e64e79e1b8722405c800e39a9adcc7511
Vilius Sutkus <ViliusSutkus89@gmail.com>
2019-11-22 21:41:06 +0000

    [CI] Clean up built dependencies before artifacting for faster cache'ing

---
+++ .github/prepareBuiltDependenciesForArtifacting.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+set -eu
+shopt -s failglob
+
+THIS_FILE=$(readlink -f "$0")
+BASEDIR=$(dirname "$THIS_FILE")/..
+
+for abi in $BASEDIR/dependency-builder/.cxx/cmake/release/*/installed
+do
+  find $abi -mindepth 1 -maxdepth 1 -not \( -name 'bin' -o -name 'include' -o -name 'lib' -o -name 'share' \) -exec rm -r {} \;
+
+  find $abi/bin -type f -not -name 'pkg-config' -exec rm {} \;
+
+  find $abi/share -mindepth 1 -maxdepth 1 -type d -not \( -name 'pdf2htmlEX' -o -name 'poppler' -o -name 'pkgconfig' \) -exec rm -r {} \;
+done
+
