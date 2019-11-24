1dcc7fd16552f9ce74f84de99bf22babe084b923
Vilius Sutkus <ViliusSutkus89@gmail.com>
2019-11-24 04:12:02 +0000

    Update pdf2htmlEX upstream dependency to v0.18.7-poppler-0.81.0 (current)

--- dependency-builder/src/main/cpp/packages/pdf2htmlEX.cmake
+++ dependency-builder/src/main/cpp/packages/pdf2htmlEX.cmake
@@ -3,7 +3,7 @@ include_guard(GLOBAL)
 ExternalProjectCMake(pdf2htmlEX
   DEPENDS cairo freetype libfontforge poppler
 
-  URL https://github.com/pdf2htmlEX/pdf2htmlEX/archive/v0.17.0-poppler-0.68.0-ubuntu-18.10.tar.gz
-  URL_HASH SHA256=399ae174fde94c1c0a4d9e4a6d8356b600589a2f33aef4091a0dd94516d2489f
+  URL https://github.com/pdf2htmlEX/pdf2htmlEX/archive/v0.18.7-poppler-0.81.0.tar.gz
+  URL_HASH SHA256=510d9fc2175fda1ab6968c389fa78d7078183583a543019f4eefe09b8373c6e4
 )
 
