f2e3d90fad4b11d6dec93ed32eb3840cc75501da
Vilius Sutkus <ViliusSutkus89@gmail.com>
2019-11-25 06:08:29 +0000

    Download freetype from sourceforge

--- dependency-builder/src/main/cpp/packages/freetype.cmake
+++ dependency-builder/src/main/cpp/packages/freetype.cmake
@@ -2,7 +2,7 @@ include_guard(GLOBAL)
 
 ExternalProjectAutotools(freetype
   DEPENDS libpng zlib
-  URL https://download.savannah.gnu.org/releases/freetype/freetype-2.10.1.tar.xz
+  URL https://download.sourceforge.net/freetype/freetype-2.10.1.tar.xz
   URL_HASH SHA256=16dbfa488a21fe827dc27eaf708f42f7aa3bb997d745d31a19781628c36ba26f
 
   CONFIGURE_ARGUMENTS --with-zlib=yes --with-bzip2=no
