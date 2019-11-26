4c7171f1836b29a7b90a5a703d70c22633c63610
Vilius Sutkus <ViliusSutkus89@gmail.com>
2019-11-26 07:44:29 +0000

    Update libffi to 3.3 from 3.3-rc0. rc was needed to build armeabi-v7a

--- dependency-builder/src/main/cpp/packages/libffi.cmake
+++ dependency-builder/src/main/cpp/packages/libffi.cmake
@@ -1,11 +1,7 @@
 include_guard(GLOBAL)
 
-# libffi-3.2.1 fails to build on armeabi-v7a ABI
-# https://github.com/libffi/libffi/issues/478
-# Use 3.3-rc0
-
 ExternalProjectAutotools(libffi
-  URL https://github.com/libffi/libffi/releases/download/v3.3-rc0/libffi-3.3-rc0.tar.gz
-  URL_HASH SHA256=403d67aabf1c05157855ea2b1d9950263fb6316536c8c333f5b9ab1eb2f20ecf
+  URL https://github.com/libffi/libffi/releases/download/v3.3/libffi-3.3.tar.gz
+  URL_HASH SHA256=72fba7922703ddfa7a028d513ac15a85c8d54c8d67f55fa5a4802885dc652056
 )
 
