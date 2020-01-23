079cbcc4a599334594db0a4475b7862ba37d7b9d
Vilius Sutkus <ViliusSutkus89@gmail.com>
2020-01-23 03:25:16 +0000

    Download Pango over FTP, not HTTPS. Expired cert. We verify hash of downloaded file anyway.

--- dependency-builder/src/main/cpp/packages/pango.cmake
+++ dependency-builder/src/main/cpp/packages/pango.cmake
@@ -2,7 +2,8 @@ include_guard(GLOBAL)
 
 ExternalProjectMeson(pango
   DEPENDS cairo fontconfig freetype fribidi glib-2.0 harfbuzz
-  URL https://ftp.gnome.org/pub/GNOME/sources/pango/1.44/pango-1.44.7.tar.xz
+#  URL https://ftp.gnome.org/pub/GNOME/sources/pango/1.44/pango-1.44.7.tar.xz
+  URL ftp://ftp.gnome.org/pub/GNOME/sources/pango/1.44/pango-1.44.7.tar.xz
   URL_HASH SHA256=66a5b6cc13db73efed67b8e933584509f8ddb7b10a8a40c3850ca4a985ea1b1f
   CONFIGURE_ARGUMENTS -Dintrospection=false
 )
