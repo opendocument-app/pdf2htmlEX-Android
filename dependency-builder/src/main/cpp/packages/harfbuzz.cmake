7f8425e40357d950663066defc10b4ce15102123
Vilius Sutkus <ViliusSutkus89@gmail.com>
2020-05-30 23:17:00 +0300

    Update Harfbuzz to 2.6.6. Use Meson to build it, removes patches.

--- dependency-builder/src/main/cpp/packages/harfbuzz.cmake
+++ dependency-builder/src/main/cpp/packages/harfbuzz.cmake
@@ -1,23 +1,8 @@
 include_guard(GLOBAL)
 
-IF (BUILD_3RDPARTY_BINARIES)
-  SET(HARFBUZZ_BUILD_UTILS_ARGUMENT -DHB_BUILD_UTILS=ON)
-ELSE()
-  SET(HARFBUZZ_BUILD_UTILS_ARGUMENT -DHB_BUILD_UTILS=OFF)
-ENDIF ()
-
-ExternalProjectCMake(harfbuzz
+ExternalProjectMeson(harfbuzz
   DEPENDS cairo fontconfig freetype glib-2.0
-  URL https://github.com/harfbuzz/harfbuzz/releases/download/2.6.4/harfbuzz-2.6.4.tar.xz
-  URL_HASH SHA256=9413b8d96132d699687ef914ebb8c50440efc87b3f775d25856d7ec347c03c12
-
-  CONFIGURE_ARGUMENTS 
-    -DHB_HAVE_FREETYPE=ON
-    -DHB_HAVE_GLIB=ON
-    -DHB_HAVE_ICU=OFF
-
-    -DHB_BUILD_TESTS=OFF
-
-    ${HARFBUZZ_BUILD_UTILS_ARGUMENTS}
+  URL https://github.com/harfbuzz/harfbuzz/releases/download/2.6.6/harfbuzz-2.6.6.tar.xz
+  URL_HASH SHA256=84d0f1fb4cf4b3ee398ac20eaa608ca9f7cd90d992a44540fdcb16469bb460e5
 )
 
