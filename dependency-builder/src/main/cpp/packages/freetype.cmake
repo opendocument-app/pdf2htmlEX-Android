64afd9c366536ee72ab7299177e94b08bb4b9818
Vilius Sutkus <ViliusSutkus89@gmail.com>
2020-05-30 22:44:10 +0300

    Update Freetype to 2.10.2, patch no longer needed

--- dependency-builder/src/main/cpp/packages/freetype.cmake
+++ dependency-builder/src/main/cpp/packages/freetype.cmake
@@ -1,9 +1,13 @@
 include_guard(GLOBAL)
 
+# Freetype needs to be built with Autotools, because CMake build
+# produces "freetype-config.cmake" alongside the pkg-config.pc file.
+# freetype-config.cmake produces errors when included in fontconfig
+
 ExternalProjectAutotools(freetype
   DEPENDS libpng zlib
-  URL https://download.sourceforge.net/freetype/freetype-2.10.1.tar.xz
-  URL_HASH SHA256=16dbfa488a21fe827dc27eaf708f42f7aa3bb997d745d31a19781628c36ba26f
+  URL https://download.savannah.gnu.org/releases/freetype/freetype-2.10.2.tar.xz
+  URL_HASH SHA512=cf45089bd8893d7de2cdcb59d91bbb300e13dd0f0a9ef80ed697464ba7aeaf46a5a81b82b59638e6b21691754d8f300f23e1f0d11683604541d77f0f581affaa
 
   CONFIGURE_ARGUMENTS --with-zlib=yes --with-bzip2=no
     --with-png=yes
