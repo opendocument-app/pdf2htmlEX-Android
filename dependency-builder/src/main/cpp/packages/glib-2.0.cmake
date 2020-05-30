f665035722704a0edc1463d0ad5d4ae2a8dff271
Vilius Sutkus <ViliusSutkus89@gmail.com>
2020-05-30 19:43:52 +0300

    Update GLib to 2.62.6. Rework stpcpy patch

--- dependency-builder/src/main/cpp/packages/glib-2.0.cmake
+++ dependency-builder/src/main/cpp/packages/glib-2.0.cmake
@@ -2,8 +2,10 @@ include_guard(GLOBAL)
 
 ExternalProjectMeson(glib-2.0
   DEPENDS iconv intl libffi zlib
-  URL https://download.gnome.org/sources/glib/2.62/glib-2.62.3.tar.xz
-  URL_HASH SHA256=4400adc9f0d3ffcfe8e84225210370ce3f9853afb81812ddadb685325aa655c4
+
+  URL https://download.gnome.org/sources/glib/2.62/glib-2.62.6.tar.xz
+  URL_HASH SHA256=104fa26fbefae8024ff898330c671ec23ad075c1c0bce45c325c6d5657d58b9c
+
   CONFIGURE_ARGUMENTS -Dlibmount=false
 )
 
