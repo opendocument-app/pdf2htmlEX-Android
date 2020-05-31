35df0779b30e7c2d6596cc895a69c149b7418365
Vilius Sutkus <ViliusSutkus89@gmail.com>
2020-05-31 15:25:35 +0300

    [CI/CD] Update scripts. use new Android cmdline tools (6514223)

--- .github/installAndroidTools.sh
+++ .github/installAndroidTools.sh
@@ -1,19 +1,18 @@
 #!/bin/sh
 set -eu
 
-if test ! -f $ANDROID_HOME/tools/bin/sdkmanager
+if test ! -f $ANDROID_HOME/cmdline-tools/tools/bin/sdkmanager
 then
-  if test ! -f android-sdk/android-sdk.zip
+  cmdtoolsfile=commandlinetools-linux-${ANDROID_SDK_TOOLS}_latest.zip
+  if test ! -f ${cmdtoolsfile}
   then
-    mkdir --parents android-sdk
-    wget --quiet --output-document=android-sdk/android-sdk.zip https://dl.google.com/android/repository/sdk-tools-linux-$ANDROID_SDK_TOOLS.zip
+    wget --quiet "https://dl.google.com/android/repository/${cmdtoolsfile}" --output-document "${cmdtoolsfile}"
   fi
-  sudo unzip -d $ANDROID_HOME android-sdk/android-sdk.zip > /dev/null
+  sudo unzip -d $ANDROID_HOME/cmdline-tools ${cmdtoolsfile} > /dev/null
 fi
 
-echo "y" | sudo $ANDROID_HOME/tools/bin/sdkmanager "tools" > /dev/null
-echo "y" | sudo $ANDROID_HOME/tools/bin/sdkmanager --licenses > /dev/null
+echo "y" | sudo $ANDROID_HOME/cmdline-tools/tools/bin/sdkmanager "tools" > /dev/null
+echo "y" | sudo $ANDROID_HOME/cmdline-tools/tools/bin/sdkmanager --licenses > /dev/null
 
-echo "y" | sudo $ANDROID_HOME/tools/bin/sdkmanager "cmake;$ANDROID_CMAKE" > /dev/null
-echo "y" | sudo $ANDROID_HOME/tools/bin/sdkmanager "ndk;$ANDROID_NDK" > /dev/null
-
+echo "y" | sudo $ANDROID_HOME/cmdline-tools/tools/bin/sdkmanager "cmake;$ANDROID_CMAKE" > /dev/null
+echo "y" | sudo $ANDROID_HOME/cmdline-tools/tools/bin/sdkmanager "ndk;$ANDROID_NDK" > /dev/null
