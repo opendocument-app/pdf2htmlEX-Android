fc9d7f89f916a8ed44d2895117177286bc3dc056
Vilius Sutkus <ViliusSutkus89@gmail.com>
2020-06-08 17:11:48 +0300

    [pdf2htmlEX_exe] Append LD_LIBRARY_PATH, if exists, not overwrite

--- pdf2htmlEX/src/main/java/com/viliussutkus89/android/pdf2htmlex/pdf2htmlEX_exe.java
+++ pdf2htmlEX/src/main/java/com/viliussutkus89/android/pdf2htmlex/pdf2htmlEX_exe.java
@@ -52,7 +52,10 @@ public class pdf2htmlEX_exe extends pdf2htmlEX {
     super(ctx, null);
 
     String nativeLibraryDir = ctx.getApplicationInfo().nativeLibraryDir;
-    String LD_LIBRARY_PATH = m_environment.put("LD_LIBRARY_PATH", nativeLibraryDir);
+    String oldLibraryPath = m_environment.put("LD_LIBRARY_PATH", nativeLibraryDir);
+    if (null != oldLibraryPath) {
+      m_environment.put("LD_LIBRARY_PATH", oldLibraryPath + ":" + nativeLibraryDir);
+    }
 
     File externalExecutable = new File(nativeLibraryDir, "libpdf2htmlEX-exe_not_lib.so");
 
