cecabd3828ede0608af3e778677912f4d127eb2c
Vilius Sutkus <ViliusSutkus89@gmail.com>
2020-06-08 18:00:53 +0300

    Remove pdf2htmlEX source patch script. Patch files will be handled by the system

--- dependency-builder/src/main/cpp/packages/pdf2htmlEX-Patch-Source.sh
+++ dependency-builder/src/main/cpp/packages/pdf2htmlEX-Patch-Source.sh
@@ -4,9 +4,15 @@ set -eu
 THIS_FILE=$(readlink -f "$0")
 BASEDIR=$(dirname "$THIS_FILE")
 
+cp $1/CMakeLists.txt $1/CMakeLists.txt.orig
+cp $1/src/pdf2htmlEX.cc $1/src/pdf2htmlEX.cc.orig
+
 # pdf2htmlEX provides a binary executable.
 # pdf2htmlEX-Android needs a library.
+# @TODO: retVal in EXE,
 patch -p0 <$BASEDIR/pdf2htmlEX-Patch-Source-make-a-library.patch
+
+patch -p0 <$BASEDIR/pdf2htmlEX-Patch-Source-prepare-exe-for-android.patch
 
 # Check for package cairo-svg, not for cairo, which could have svg headers.
 patch $1/CMakeLists.txt <$BASEDIR/pdf2htmlEX-Patch-Source-find-cairo-svg.patch
