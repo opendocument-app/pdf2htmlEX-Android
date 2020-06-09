84bfd733ada5816f1632a278e116aa3f46f7d1a3
Vilius Sutkus <ViliusSutkus89@gmail.com>
2020-06-09 12:57:39 +0300

    Update GLib to 2.64.3. No need to explicitly disable libmount anymore.

--- dependency-builder/src/main/cpp/packages/glib-2.0.cmake
+++ dependency-builder/src/main/cpp/packages/glib-2.0.cmake
@@ -3,9 +3,7 @@ include_guard(GLOBAL)
 ExternalProjectMeson(glib-2.0
   DEPENDS iconv intl libffi zlib
 
-  URL https://download.gnome.org/sources/glib/2.62/glib-2.62.6.tar.xz
-  URL_HASH SHA256=104fa26fbefae8024ff898330c671ec23ad075c1c0bce45c325c6d5657d58b9c
-
-  CONFIGURE_ARGUMENTS -Dlibmount=false
+  URL https://download.gnome.org/sources/glib/2.64/glib-2.64.3.tar.xz
+  URL_HASH SHA256=fe9cbc97925d14c804935f067a3ad77ef55c0bbe9befe68962318f5a767ceb22
 )
 
