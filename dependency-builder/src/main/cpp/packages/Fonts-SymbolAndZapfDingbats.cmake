ea535b491cd6d2f408cfd44a6ed747243d3b0ee8
Vilius Sutkus '89 <ViliusSutkus89@gmail.com>
2020-12-17 18:15:15 +0200

    Update Symbol and Zapf Dingbats download link

--- dependency-builder/src/main/cpp/packages/Fonts-SymbolAndZapfDingbats.cmake
+++ dependency-builder/src/main/cpp/packages/Fonts-SymbolAndZapfDingbats.cmake
@@ -3,7 +3,7 @@ include_guard(GLOBAL)
 # https://www.xpdfreader.com/download.html
 
 ExternalProjectTarball(Fonts-SymbolAndZapfDingbats
-  URL https://xpdfreader-dl.s3.amazonaws.com/xpdf-t1fonts.tar.gz
+  URL https://dl.xpdfreader.com/xpdf-t1fonts.tar.gz
   URL_HASH SHA256=513853afed2a4ada0217bd93d95ab7f1663b6a0f37a07136e22404c6c6eaf342
 )
 
