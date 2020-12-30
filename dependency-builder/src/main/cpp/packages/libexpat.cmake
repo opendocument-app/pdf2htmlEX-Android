1160ee8f534b7b2358498b5962f8cb196c81ceaf
Vilius Sutkus '89 <ViliusSutkus89@gmail.com>
2020-12-30 19:53:09 +0200

    [dependencies] Add libexpat, will be used by fontconfig

---
+++ dependency-builder/src/main/cpp/packages/libexpat.cmake
@@ -0,0 +1,7 @@
+include_guard(GLOBAL)
+
+ExternalProjectAutotools(libexpat
+  URL https://github.com/libexpat/libexpat/releases/download/R_2_2_10/expat-2.2.10.tar.xz
+  URL_HASH SHA256=5dfe538f8b5b63f03e98edac520d7d9a6a4d22e482e5c96d4d06fcc5485c25f2
+)
+
