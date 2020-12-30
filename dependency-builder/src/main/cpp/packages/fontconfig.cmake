26da291187ae5db4c79c477d6b5dcbdc6e900c3f
Vilius Sutkus '89 <ViliusSutkus89@gmail.com>
2020-12-30 19:57:09 +0200

    [dependencies] Update fontconfig to 2.13.93, change libxml2 dep to libexpat, remove dep on iconv

--- dependency-builder/src/main/cpp/packages/fontconfig.cmake
+++ dependency-builder/src/main/cpp/packages/fontconfig.cmake
@@ -1,12 +1,14 @@
 include_guard(GLOBAL)
 
-ExternalProjectAutotools(fontconfig
-  DEPENDS freetype iconv libxml-2.0
-  URL https://www.freedesktop.org/software/fontconfig/release/fontconfig-2.13.92.tar.xz
-  URL_HASH SHA256=506e61283878c1726550bc94f2af26168f1e9f2106eac77eaaf0b2cdfad66e4e
+ExternalProjectMeson(fontconfig
+  DEPENDS freetype libexpat
+  URL https://www.freedesktop.org/software/fontconfig/release/fontconfig-2.13.93.tar.xz
+  URL_HASH SHA256=ea968631eadc5739bc7c8856cef5c77da812d1f67b763f5e51b57b8026c1a0a0
 
   CONFIGURE_ARGUMENTS
-    --disable-docs
-    --enable-libxml2
+    -Dnls=enabled
+    -Ddoc=disabled
+    -Dtests=disabled
+    -Dtools=disabled
 )
 
