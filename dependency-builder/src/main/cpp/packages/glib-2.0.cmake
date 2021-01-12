1b35bf463e34567a68c80e12502f0082fada227b
Vilius Sutkus '89 <ViliusSutkus89@gmail.com>
2021-01-12 11:49:09 +0200

    [dependencies] Update GLib to 2.66.4

--- dependency-builder/src/main/cpp/packages/glib-2.0.cmake
+++ dependency-builder/src/main/cpp/packages/glib-2.0.cmake
@@ -3,7 +3,7 @@ include_guard(GLOBAL)
 ExternalProjectMeson(glib-2.0
   DEPENDS iconv intl libffi zlib
 
-  URL https://download.gnome.org/sources/glib/2.64/glib-2.64.3.tar.xz
-  URL_HASH SHA256=fe9cbc97925d14c804935f067a3ad77ef55c0bbe9befe68962318f5a767ceb22
+  URL https://download.gnome.org/sources/glib/2.66/glib-2.66.4.tar.xz
+  URL_HASH SHA256=97df8670e32f9fd4f7392b0980e661dd625012015d58350da1e58e343f4af984
 )
 
