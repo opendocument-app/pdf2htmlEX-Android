88da954a52be67e48c641c8fcf54128c11b063b4
Vilius Sutkus '89 <ViliusSutkus89@gmail.com>
2021-01-16 13:58:38 +0200

    [Dependencies] Update Cairo to 1.17.4

--- dependency-builder/src/main/cpp/packages/cairo.cmake
+++ dependency-builder/src/main/cpp/packages/cairo.cmake
@@ -2,8 +2,8 @@ include_guard(GLOBAL)
 
 ExternalProjectAutotools(cairo
   DEPENDS fontconfig freetype glib-2.0 iconv libpng libxml-2.0 pixman-1
-  URL https://cairographics.org/snapshots/cairo-1.17.2.tar.xz
-  URL_HASH SHA256=6b70d4655e2a47a22b101c666f4b29ba746eda4aa8a0f7255b32b2e9408801df
+  URL https://cairographics.org/snapshots/cairo-1.17.4.tar.xz
+  URL_HASH SHA256=74b24c1ed436bbe87499179a3b27c43f4143b8676d8ad237a6fa787401959705
   CONFIGURE_ARGUMENTS --enable-xml
 )
 
