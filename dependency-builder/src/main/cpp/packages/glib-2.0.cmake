1797d5d267262767996ba1e852f6360820d12856
Vilius Sutkus '89 <ViliusSutkus89@gmail.com>
2021-01-19 00:15:43 +0200

    [Dependencies] Don't build libintl-proxy. Let glib build it.
    GLib doesn't detect prebuilt libintl-proxy and builds it anyway.

--- dependency-builder/src/main/cpp/packages/glib-2.0.cmake
+++ dependency-builder/src/main/cpp/packages/glib-2.0.cmake
@@ -1,7 +1,7 @@
 include_guard(GLOBAL)
 
 ExternalProjectMeson(glib-2.0
-  DEPENDS iconv intl libffi zlib
+  DEPENDS iconv libffi zlib #intl
 
   URL https://download.gnome.org/sources/glib/2.66/glib-2.66.4.tar.xz
   URL_HASH SHA256=97df8670e32f9fd4f7392b0980e661dd625012015d58350da1e58e343f4af984
--- dependency-builder/src/main/cpp/packages/intl.cmake
+++ dependency-builder/src/main/cpp/packages/intl.cmake
@@ -1,5 +1,7 @@
 include_guard(GLOBAL)
 
+message(FATAL_ERROR "libintl-proxy package is disabled")
+
 ExternalProjectMeson(intl
   URL https://github.com/ViliusSutkus89/proxy-libintl/archive/0.2.tar.gz
   URL_HASH SHA256=9467f672d2e18d61fb14cbfb08d4afcc1f88c0ec372cac79bc19e848e8d66ba7
