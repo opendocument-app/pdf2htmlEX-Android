7ebb70f4ced36475200c2ffb8c57bedbb05f86d0
Vilius Sutkus '89 <ViliusSutkus89@gmail.com>
2021-07-29 03:17:02 +0300

    [SampleApp] Refactor deprecated open/save pdf intents to use androidx.activity

--- android-sample-app/app/src/main/java/com/viliussutkus89/android/pdf2htmlex/sampleapp/MainActivity.java
+++ android-sample-app/app/src/main/java/com/viliussutkus89/android/pdf2htmlex/sampleapp/MainActivity.java
@@ -1,16 +1,17 @@
 package com.viliussutkus89.android.pdf2htmlex.sampleapp;
 
-import android.app.Activity;
+import android.content.ActivityNotFoundException;
+import android.content.ContentResolver;
 import android.content.Context;
 import android.content.Intent;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.OpenableColumns;
-import android.util.Log;
-import android.view.View;
 import android.widget.Toast;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.FileProvider;
 
@@ -21,26 +22,88 @@ import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 
 public class MainActivity extends AppCompatActivity {
-
-    static final String TAG = "P2H";
-
-    static final int INTENT_OPEN = 1;
-    static final int INTENT_SAVE = 2;
-    static final int INTENT_SAVE_RESULT_FILENAME = 3;
-
     // cacheDir is where this Android App stores incoming .pdf
     private File m_inputDir;
     // outputDir is where produced .html's will be stored
     private File m_outputDir;
 
     private File m_convertedHTMLWaitingToBeSaved = null;
+
+    private final ActivityResultLauncher<String> m_openPdfForReading = registerForActivityResult(new ActivityResultContracts.GetContent(),
+            selectedInputDocument -> {
+                Context ctx = getApplicationContext();
+                File html;
+                try {
+                    html = convertPdfToHTML(ctx, selectedInputDocument);
+                } catch (IOException | pdf2htmlEX.ConversionFailedException e) {
+                    Toast.makeText(ctx, "Conversion failed!", Toast.LENGTH_LONG).show();
+                    return;
+                }
+
+                File htmlInOutputFolder = new File(m_outputDir, html.getName());
+                html.renameTo(htmlInOutputFolder);
+
+                String authority = ctx.getPackageName() + ".provider";
+                Uri apkUri = FileProvider.getUriForFile(ctx, authority, htmlInOutputFolder);
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                intent.addCategory(Intent.CATEGORY_BROWSABLE);
+                intent.setDataAndType(apkUri, "text/html");
+                intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+                try {
+                    startActivity(intent);
+                } catch (ActivityNotFoundException e) {
+                    Toast.makeText(ctx, "HTML document generated, but failed to open HTML reader!", Toast.LENGTH_LONG).show();
+                }
+            });
+
+    private final ActivityResultLauncher<String> m_saveHTML = registerForActivityResult(new ActivityResultContracts.CreateDocument(),
+            selectedOutputDocument -> {
+                try {
+                    InputStream input = new FileInputStream(m_convertedHTMLWaitingToBeSaved);
+                    OutputStream output = getContentResolver().openOutputStream(selectedOutputDocument);
+                    copyFile(input, output);
+                    m_convertedHTMLWaitingToBeSaved.delete();
+                } catch (IOException e) {
+                    Toast.makeText(getApplicationContext(), "Failed to save HTML document!", Toast.LENGTH_LONG).show();
+                }
+                m_convertedHTMLWaitingToBeSaved = null;
+            });
+
+    private final ActivityResultLauncher<String> m_openPdfForSaving = registerForActivityResult(new ActivityResultContracts.GetContent(),
+            selectedInputDocument -> {
+                Context ctx = getApplicationContext();
+                File html;
+                try {
+                    html = convertPdfToHTML(ctx, selectedInputDocument);
+                } catch (IOException | pdf2htmlEX.ConversionFailedException e) {
+                    Toast.makeText(ctx, "Conversion failed!", Toast.LENGTH_LONG).show();
+                    return;
+                }
+                m_convertedHTMLWaitingToBeSaved = html;
+                m_saveHTML.launch(m_convertedHTMLWaitingToBeSaved.getName());
+            });
+
+    // @TODO: should be in non-GUI thread
+    private File convertPdfToHTML(Context ctx, Uri input) throws IOException, pdf2htmlEX.ConversionFailedException {
+        String filename = getFileName(ctx.getContentResolver(), input);
+        File pdf_in_cache = new File(m_inputDir, filename);
+
+        InputStream inputStream = getContentResolver().openInputStream(input);
+        OutputStream outputStream = new FileOutputStream(pdf_in_cache);
+        copyFile(inputStream, outputStream);
+
+        pdf2htmlEX converter = new pdf2htmlEX(ctx);
+        converter.setInputPDF(pdf_in_cache);
+        File html = converter.convert();
+        pdf_in_cache.delete();
+        return html;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -53,161 +116,41 @@ public class MainActivity extends AppCompatActivity {
         // Must be defined in provider_paths.xml
         m_outputDir = new File(getCacheDir(), "produced-htmls");
         m_outputDir.mkdir();
+
+        findViewById(R.id.button_open).setOnClickListener(view -> m_openPdfForReading.launch("application/pdf"));
+        findViewById(R.id.button_save).setOnClickListener(view -> m_openPdfForSaving.launch("application/pdf"));
+        findViewById(R.id.button_licenses).setOnClickListener(view -> startActivity(new Intent(this, OssLicensesMenuActivity.class)));
     }
 
-    private Boolean copyFile(InputStream input, OutputStream output) {
+    private void copyFile(InputStream input, OutputStream output) throws IOException {
         final int buffer_size = 1024;
         byte[] buffer = new byte[buffer_size];
 
-        BufferedInputStream in = new BufferedInputStream(input, buffer_size);
-        BufferedOutputStream out = new BufferedOutputStream(output, buffer_size);
-
-        try {
-            int read;
-            while (-1 != (read = in.read(buffer))) {
-                out.write(buffer, 0, read);
-            }
-            out.flush();
-            out.close();
-            output.close();
-            in.close();
-            input.close();
-        } catch (IOException e) {
-            Log.e(TAG, e.getMessage());
-            return false;
-        }
-        return true;
-    }
-
-    // https://stackoverflow.com/questions/5568874/how-to-extract-the-file-name-from-uri-returned-from-intent-action-get-content
-    private String getFileName(Uri uri) {
-        String result = null;
-        if (uri.getScheme().equals("content")) {
-            Cursor cursor = getContentResolver().query(uri, null, null, null, null);
-            try {
-                if (null != cursor && cursor.moveToFirst()) {
-                    result = cursor.getString(cursor.getColumnIndex(OpenableColumns.DISPLAY_NAME));
+        try (BufferedInputStream in = new BufferedInputStream(input, buffer_size)) {
+            try (BufferedOutputStream out = new BufferedOutputStream(output, buffer_size)) {
+                int read;
+                while (-1 != (read = in.read(buffer))) {
+                    out.write(buffer, 0, read);
                 }
+                out.flush();
             } finally {
-                cursor.close();
+                output.close();
             }
-        }
-        if (null == result) {
-            result = uri.getPath();
-            int cut = result.lastIndexOf('/');
-            if (-1 != cut) {
-                result = result.substring(cut + 1);
-            }
-        }
-        return result;
-    }
-
-    @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent resultData) {
-        super.onActivityResult(requestCode, resultCode, resultData);
-
-        if (Activity.RESULT_OK != resultCode || null == resultData) {
-            return;
-        }
-        if (INTENT_OPEN != requestCode && INTENT_SAVE != requestCode && INTENT_SAVE_RESULT_FILENAME != requestCode) {
-            return;
-        }
-
-        /* Copy out the file to cache first.
-            NDK side will have to open it and it won't go through the Android Content APIs...
-            @TODO: Patch poppler to allow passing fileDescriptor instead of a filepath
-         */
-        Uri uri = resultData.getData();
-
-        if (INTENT_SAVE_RESULT_FILENAME == requestCode) {
-            try {
-                if (null != m_convertedHTMLWaitingToBeSaved && m_convertedHTMLWaitingToBeSaved.exists()) {
-                    InputStream input = new FileInputStream(m_convertedHTMLWaitingToBeSaved);
-                    OutputStream output = getContentResolver().openOutputStream(uri);
-                    if (!copyFile(input, output)) {
-                        return;
-                    }
-                    m_convertedHTMLWaitingToBeSaved.delete();
-                    m_convertedHTMLWaitingToBeSaved = null;
-                    findViewById(R.id.button_save).setEnabled(true);
-                }
-            } catch (FileNotFoundException e) {
-                return;
-            }
-            return;
-        }
-
-        String filename = getFileName(uri);
-        File pdf_in_cache = new File(m_inputDir, filename);
-        try {
-            InputStream input = getContentResolver().openInputStream(uri);
-            OutputStream output = new FileOutputStream(pdf_in_cache);
-            if (!copyFile(input, output)) {
-                return;
-            }
-        } catch (FileNotFoundException e) {
-            return;
-        }
-
-        Context ctx = getApplicationContext();
-        pdf2htmlEX converter = new pdf2htmlEX(ctx);
-
-        File html;
-        try {
-            // @TODO: should be in non-GUI thread
-            html = converter.convert(pdf_in_cache);
-        } catch (Exception e) {
-            Toast.makeText(ctx, "Conversion failed: " + e.getMessage(), Toast.LENGTH_LONG).show();
-            pdf_in_cache.delete();
-            return;
-        }
-
-        pdf_in_cache.delete();
-
-        if (html.exists()) {
-            if (INTENT_OPEN == requestCode) {
-                File htmlInOutputFolder = new File(m_outputDir, html.getName());
-                html.renameTo(htmlInOutputFolder);
-
-                findViewById(R.id.button_open).setEnabled(true);
-
-                String authority = ctx.getPackageName() + ".provider";
-                Uri apkUri = FileProvider.getUriForFile(ctx, authority, htmlInOutputFolder);
-                Intent intent = new Intent(Intent.ACTION_VIEW);
-                intent.addCategory(Intent.CATEGORY_BROWSABLE);
-                intent.setDataAndType(apkUri, "text/html");
-                intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
-                startActivity(intent);
-            }
-            else if (INTENT_SAVE == requestCode) {
-                // @TODO: could this variable be somehow passed through intent?
-                m_convertedHTMLWaitingToBeSaved = html;
-
-                Intent intent = new Intent(Intent.ACTION_CREATE_DOCUMENT);
-                intent.setType("text/html");
-                intent.putExtra(Intent.EXTRA_TITLE, filename + ".html");
-                startActivityForResult(intent, INTENT_SAVE_RESULT_FILENAME);
-            }
+        } finally {
+            input.close();
         }
     }
 
-    public void handleOpenButton(View v) {
-        v.setEnabled(false);
-        Intent openPDFIntent = new Intent(Intent.ACTION_GET_CONTENT);
-        openPDFIntent.addCategory(Intent.CATEGORY_OPENABLE);
-        openPDFIntent.setType("application/pdf");
-        startActivityForResult(openPDFIntent, INTENT_OPEN);
-    }
-
-    public void handleSaveButton(View v) {
-        v.setEnabled(false);
-        Intent openPDFIntent = new Intent(Intent.ACTION_GET_CONTENT);
-        openPDFIntent.addCategory(Intent.CATEGORY_OPENABLE);
-        openPDFIntent.setType("application/pdf");
-        startActivityForResult(openPDFIntent, INTENT_SAVE);
-    }
-
-    public void handleShowLicenses(View v) {
-        startActivity(new Intent(this, OssLicensesMenuActivity.class));
+    // https://developer.android.com/training/secure-file-sharing/retrieve-info
+    private String getFileName(ContentResolver contentResolver, Uri uri) {
+        try (Cursor cursor = contentResolver.query(uri, null, null, null, null)) {
+            cursor.moveToFirst();
+            int nameIndex = cursor.getColumnIndex(OpenableColumns.DISPLAY_NAME);
+            if (nameIndex < 0) {
+                // This should not happen, but checking anyway to make the lint happy.
+                return "UnknownFile";
+            }
+            return cursor.getString(nameIndex);
+        }
     }
 }
