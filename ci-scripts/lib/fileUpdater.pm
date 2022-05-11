8fc92ae5940d3810d4af1f3d0834fd4cca43e5b0
Vilius Sutkus '89 <ViliusSutkus89@gmail.com>
2022-05-11 09:57:12 +0300

    Update CI template, Bump AGP to 7.2.0

--- ci-scripts/lib/fileUpdater.pm
+++ ci-scripts/lib/fileUpdater.pm
@@ -1,9 +1,9 @@
 #  MIT License
 #
-#  Copyright (c) 2021 Vilius Sutkus <ViliusSutkus89@gmail.com>
+#  Copyright (c) 2021 - 2022 ViliusSutkus89.com
 #
 #  https://github.com/ViliusSutkus89/Sample_Android_Library-MavenCentral-Instrumented_Tests
-#  ci-scripts/lib/fileParser.pm - v1.1.5
+#  ci-scripts/lib/fileParser.pm - v2.1.0
 #
 #  Permission is hereby granted, free of charge, to any person obtaining a copy
 #  of this software and associated documentation files (the "Software"), to deal
@@ -28,7 +28,7 @@ use warnings;
 use strict;
 
 use base 'Exporter';
-our $VERSION = '1.01';
+our $VERSION = '2.1.0';
 
 use File::Find 'find';
 use File::Path 'make_path';
@@ -94,6 +94,52 @@ sub update {
     }
     elsif (-f _) {
         $self->__updateSingleFile($inputFile, $lineUpdateExpressionsRef, $filenameUpdateExpressionsRef);
+    }
+}
+
+sub append {
+    my $self = shift;
+    my $input = shift;
+
+    my $lineToAppend = shift;
+    my $recursion = sub {
+        $self->append(shift, $lineToAppend);
+    };
+
+    if ('ARRAY' eq ref($input)) {
+        foreach my $file (@$input) {
+            &$recursion($file);
+        }
+        return;
+    }
+    elsif ('' ne ref($input)) {
+        use Data::Dumper;
+        die('Unrecognized input: ' . Dumper($input));
+    }
+
+    my $inputFile = getAbsolutePath($input, {
+        pathRelativeTo                => $self->{rootDirectory},
+        doResolvePathAndCheckIfExists => 1
+    });
+
+    -e $inputFile or die("Input $inputFile does not exist!\n");
+    if (-d _) {
+        File::Find::find(sub {
+            &$recursion($File::Find::name) if (-f $File::Find::name);
+        }, $inputFile);
+    }
+    elsif (-f _) {
+        my $outputFile = $inputFile;
+        my $inPlaceEdit = !$self->{outputDirectory};
+        if ($inPlaceEdit != 1) {
+            $outputFile = $self->{outputDirectory} . substr($inputFile, length($self->{rootDirectory}));
+        }
+
+        $self->__updateSingleFile($inputFile) if ! -e $outputFile;
+
+        open(my $FH_OUTPUT, '>>', $outputFile) or die "$! : $outputFile\n";
+        print $FH_OUTPUT $lineToAppend;
+        close($FH_OUTPUT);
     }
 }
 
