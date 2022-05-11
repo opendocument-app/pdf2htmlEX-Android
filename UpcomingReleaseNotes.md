b44793313cf8ea4badb7d8950374e5bb85dc0767
Vilius Sutkus '89 <ViliusSutkus89@gmail.com>
2022-05-11 16:25:01 +0300

    [Deps] Lower GLib version bump to 2.66.8

--- UpcomingReleaseNotes.md
+++ UpcomingReleaseNotes.md
@@ -1,6 +1,6 @@
 Update dependencies:
 - FreeType-2.12.1
-- GLib-2.72.1
+- GLib-2.66.8
 - proxy-libintl-0.4.1
 - libffi-3.4.2
 - libxml2-2.9.14
--- dependency-builder/src/main/cpp/packages/glib-2.0.cmake
+++ dependency-builder/src/main/cpp/packages/glib-2.0.cmake
@@ -3,7 +3,7 @@ include_guard(GLOBAL)
 ExternalProjectMeson(glib-2.0
   DEPENDS iconv libffi zlib intl
 
-  URL https://download.gnome.org/sources/glib/2.72/glib-2.72.1.tar.xz
-  URL_HASH SHA256=c07e57147b254cef92ce80a0378dc0c02a4358e7de4702e9f403069781095fe2
+  URL https://download.gnome.org/sources/glib/2.66/glib-2.66.8.tar.xz
+  URL_HASH SHA256=97bc87dd91365589af5cbbfea2574833aea7a1b71840fd365ecd2852c76b9c8b
 )
 
