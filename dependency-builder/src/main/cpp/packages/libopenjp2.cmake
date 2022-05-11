d0db724998d99e637b1fb80032fac6d37eb1fbbb
Vilius Sutkus '89 <ViliusSutkus89@gmail.com>
2022-05-11 19:53:45 +0300

    [Deps] Update OpenJPEG URL

--- dependency-builder/src/main/cpp/packages/libopenjp2.cmake
+++ dependency-builder/src/main/cpp/packages/libopenjp2.cmake
@@ -2,7 +2,7 @@ include_guard(GLOBAL)
 
 ExternalProjectCMake(libopenjp2
   DEPENDS lcms2 libpng libtiff-4 libjpeg
-  URL https://github.com/uclouvain/openjpeg/releases/download/v2.4.0/openjpeg-v2.4.0-linux-x86_64.tar.gz
-  URL_HASH SHA256=0c3aae80679504fa3727f3f3779defc0746c08d83e49ef1f262baf5d916cd5f9
+  URL https://github.com/uclouvain/openjpeg/archive/refs/tags/v2.4.0.tar.gz
+  URL_HASH SHA256=8702ba68b442657f11aaeb2b338443ca8d5fb95b0d845757968a7be31ef7f16d
   CONFIGURE_ARGUMENTS -DBUILD_CODEC=OFF
 )
