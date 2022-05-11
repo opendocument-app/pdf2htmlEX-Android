2980e9876744c3ceea076631f1a067cbaa36e504
Vilius Sutkus '89 <ViliusSutkus89@gmail.com>
2022-05-11 15:16:45 +0300

    [Deps] Bump proxy-libintl to 0.4.1

--- UpcomingReleaseNotes.md
+++ UpcomingReleaseNotes.md
@@ -1,3 +1,4 @@
 Update dependencies:
 - FreeType-2.12.1
 - GLib-2.72.1
+- proxy-libintl-0.4.1
--- dependency-builder/src/main/cpp/packages/intl.cmake
+++ dependency-builder/src/main/cpp/packages/intl.cmake
@@ -1,6 +1,6 @@
 include_guard(GLOBAL)
 
 ExternalProjectMeson(intl
-  URL https://github.com/ViliusSutkus89/proxy-libintl/archive/0.2.tar.gz
-  URL_HASH SHA256=9467f672d2e18d61fb14cbfb08d4afcc1f88c0ec372cac79bc19e848e8d66ba7
+  URL https://github.com/ViliusSutkus89/proxy-libintl/archive/refs/tags/0.4.1.tar.gz
+  URL_HASH SHA256=89d72a5cdd6df29beb6cf1aa7284570ff8f4265185ecbf44827b741a864bbec9
 )
